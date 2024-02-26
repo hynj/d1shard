c2b8b8ae41f9e6d9967caaeb334e1e03e0549992
Nick Local <thenick112@gmail.com>
2024-02-26 19:49:33 +0000

    fix

---
+++ src/routes/loaderio-63cba677f74a873cfe7c8396b3eb8388/+server.ts
@@ -0,0 +1,7 @@
+import type { RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = ({ url }) => {
+  const response = new Response('loaderio-63cba677f74a873cfe7c8396b3eb8388');
+
+  return response;
+}
--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+    alias: {
+			// this will match a file
+			'loaderio-63cba677f74a873cfe7c8396b3eb8388': 'static/loaderio-63cba677f74a873cfe7c8396b3eb8388.txt'
+    },
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
